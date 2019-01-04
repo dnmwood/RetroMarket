a7fafa3f7c37f55ceed1ada67f1469b3eb60eb36
DAVID <davidnmwood@gmail.com>
2019-01-04 17:03:46 -0500

    Cleaned up routes file, setup resources for Games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,7 @@
 class GamesController < ApplicationController
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+
   def index
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,8 @@
 Rails.application.routes.draw do
-  get 'games/index'
-
-  get 'games/show'
-
-  get 'games/new'
-
-  get 'games/console_list'
-
+  # Games pages, user authentication required
+  resources :games, only: [:index, :show, :new, :console_list]
   devise_for :users
-  get 'homepage/index'
 
   root to: 'homepage#index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
